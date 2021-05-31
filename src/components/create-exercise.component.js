249feb029daf4b92ec488b39ce904150946b05e2
yudsx0915 <yudsx0915@gmail.com>
2021-05-31 22:27:51 +0800

    20210531

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css"
+import axios from 'axios';
 
 export default class CreateExercises extends Component{
     constructor(props){
@@ -22,10 +23,15 @@ export default class CreateExercises extends Component{
     }
 
     componentDidMount(){
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if(response.data.length > 0){
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e){
@@ -61,7 +67,11 @@ export default class CreateExercises extends Component{
             date: this.state.date
         }
         console.log(exercise);
-        window.location = '/';
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
+
+        //window.location = '/';
     }
 
     render(){
