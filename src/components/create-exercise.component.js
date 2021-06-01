cd2ca1485e78f262874c75fd9eeebab69b15e5b4
yudsx0915 <yudsx0915@gmail.com>
2021-06-01 16:25:32 +0800

    course finished

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -69,9 +69,10 @@ export default class CreateExercises extends Component{
         console.log(exercise);
 
         axios.post('http://localhost:5000/exercises/add', exercise)
-            .then(res => console.log(res.data));
-
-        //window.location = '/';
+            .then(res => {
+                console.log(res.data);
+                window.location = '/';
+            });
     }
 
     render(){
